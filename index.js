dd37caaffab9d7e65183bffec862c246c4c568d5
Hafiz <hafiz.dev24@gmail.com>
2025-11-25 16:15:04 +0600

    connected server with mongodb and added .env file

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -7,9 +9,32 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oquvsp8.mongodb.net/?appName=Cluster0`;
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
 app.get('/', (req, res) => {
   res.send('AI model inventory manager server is running');
 })
+
+async function run() {
+  try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }
+  finally {
+
+  }
+}
+
+run().catch(console.dir);
 
 app.listen(port, () => {
   console.log(`AI model inventory manager server is running on port: ${port}`);
