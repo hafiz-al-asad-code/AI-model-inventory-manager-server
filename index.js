60e0cfbfb87431f9e469dd0afcd4ecc95ef8257f
Hafiz <hafiz.dev24@gmail.com>
2025-12-07 01:10:32 +0600

    added '/models/:id' API to show model details

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 3000;
@@ -42,6 +42,13 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
+
+    app.get('/models/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = new ObjectId(id);
+      const result = await modelsCollection.findOne(query);
+      res.send(result);
+    })
 
     app.post('/models', async (req, res) => {
       const newModel = req.body;
